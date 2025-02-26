9843dc0584556cd6363835376fd69293a8ade565
Liubou Hadunova <liubou.hadunova@leverx.com>
2025-02-26 09:10:04 +0100

    feat: add hours to timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,24 +2,37 @@
 
 import styles from "./ui/timer.module.css";
 import { useStoreTime } from "../store/storeTime";
+import { useEffect } from "react";
 
 function displayTime(time: number) {
     const timeString = String(time).padStart(2, "0");
     return timeString;
 }
 
-function getTimeFromSeconds(time: number): [minutes: number, seconds: number] {
-    const minutes = Math.floor(time / 60);
-    const seconds = time - 60 * minutes;
-    return [minutes, seconds];
+function getTimeFromSeconds(time: number): [hours: number, minutes: number, seconds: number] {
+    const hours = Math.floor(time / 3600);
+    const remainingtime = time - hours * 3600;
+    const minutes = Math.floor(remainingtime / 60);
+    const seconds = remainingtime - 60 * minutes;
+    return [hours, minutes, seconds];
 }
 
 export function Timer() {
     const time = useStoreTime((state) => state.time);
-    const [minutes, seconds] = getTimeFromSeconds(time);
+    const [hours, minutes, seconds] = getTimeFromSeconds(time);
+    useEffect(() => {
+        let formattedTime = "";
+        if (hours > 0) {
+            formattedTime = `${displayTime(hours)}:${displayTime(minutes)}:${displayTime(seconds)}`;
+        } else {
+            formattedTime = `${displayTime(minutes)}:${displayTime(seconds)}`;
+        }
+        document.title = `${formattedTime} Countdown | Bigtimer`;
+    }, [hours, minutes, seconds]);
     return (
         <div className={styles.timer}>
-            <span>{displayTime(minutes)}</span>:<span>{displayTime(seconds)}</span>
+            {hours > 0 && <span>{displayTime(hours)} : </span>}
+            <span>{displayTime(minutes)}</span> : <span>{displayTime(seconds)}</span>
         </div>
     );
 }
